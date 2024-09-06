e17a6b27d1acba88fa555af09661f0484898bb02
Mohamed Alaa <mohamedalaa4j@gmail.com>
2024-09-06 17:57:33 +0300

    MVP HomeScreen design

--- app/src/main/java/com/example/mycalories/ui/HomeScreen.kt
+++ app/src/main/java/com/example/mycalories/ui/HomeScreen.kt
@@ -1,18 +1,22 @@
 package com.example.mycalories.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,27 +43,48 @@ fun HomeScreen(
         )
 
         Spacer(modifier = Modifier.height(16.dp))
-        FoodList(foodList)
+        FoodListView(foodList)
+
+        ButtonView()
     }
 }
 
 @Composable
-fun FoodList(foodList: List<FoodItemModel>){
-    LazyColumn (
+fun FoodListView(foodList: List<FoodItemModel>) {
+    LazyColumn(
         modifier = Modifier.fillMaxWidth()
-    ){
-        items(foodList){ food ->
+    ) {
+        items(foodList) { food ->
             FoodItemView(food = food)
         }
     }
 }
 
 @Composable
-fun FoodItemView(food: FoodItemModel){
+fun FoodItemView(food: FoodItemModel) {
     Text(
         text = "${food.name} ${food.calories} cal",
         fontSize = 14.sp
     )
+}
+
+@Composable
+fun ButtonView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+            .padding(vertical = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Button(modifier = Modifier
+            .size(70.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(fontSize = 12.sp, text = "Add")
+        }
+    }
 }
 
 @Composable
--- app/src/main/java/com/example/mycalories/ui/MainActivity.kt
+++ app/src/main/java/com/example/mycalories/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.mycalories.domain.model.getFoodList
 import com.example.mycalories.ui.theme.MyCaloriesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,10 +12,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
             MyCaloriesTheme {
-
+                HomeScreen(foodList = getFoodList())
             }
         }
     }
